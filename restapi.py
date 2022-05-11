57fdb2c9d93c92f4030b89735c4bfa0cd88d6426
Harish Akula <harish.akula32k@gmail.com>
2022-05-11 15:00:44 +0530

    Initial commit

---
+++ restapi.py
@@ -0,0 +1,51 @@
+import os
+import torch
+from flask import Flask, request, jsonify
+from PIL import Image
+from io import BytesIO
+import base64
+
+app = Flask(__name__)
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    #img_names = ['img', 'img1', 'img2', 'img3', 'img4']
+    print("received image(s)")
+    instances = request.json.get('instances')
+    
+    names = []
+    images = []
+    for instance in instances:
+        names.append(instance['name'])
+        enc_img = instance['image']
+        img_bytes = BytesIO(base64.b64decode(enc_img))
+        image = Image.open(img_bytes)
+        images.append(image)
+    
+    results = model(images, size=640)
+    results_xyxy = results.pandas().xyxy
+    outputs = []
+    for i in range(len(results_xyxy)):
+        result = results_xyxy[i]
+        output = { "detections" : eval(result.to_json(orient="records")),
+            "name" : names[i]
+        }
+        outputs.append(output)
+    
+    to_return = {'predictions' : outputs}
+    return jsonify(to_return)
+
+@app.route('/health', methods=['GET'])
+def health_check():
+   return jsonify({"status": 'healthy'})
+
+
+if __name__ == "__main__":
+    
+    # Fix known issue urllib.error.HTTPError 403: rate limit exceeded https://github.com/ultralytics/yolov5/pull/7210
+    torch.hub._validate_not_a_forked_repo = lambda a, b, c: True
+    torch.hub.set_dir(os.path.join('static'))
+
+    model = torch.hub.load("ultralytics/yolov5", "yolov5s", force_reload=False)  # force_reload to recache
+    app.run(host="0.0.0.0", port=5000)  # debug=True causes Restarting with stat
